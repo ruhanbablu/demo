49916d90a0218b7d565ad174a23e84a3b56d633e
bablu ahmed <ruhanbablu>
2021-09-05 15:48:15 +0600

    first commit

---
+++ assets/css/script.js
@@ -0,0 +1,28 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.header-content, .service-header, .service-title, .service-card-block, .count, .contact-header, .contact-title `, {
+    interval: 200
+})
+const img = ScrollReveal({
+    origin: 'right',
+    distance:'100px',
+    duration:2000,
+    reset: true
+})
+img.reveal(` .about-text, .right, .bio-img, .address`,{
+    interval:100
+})
+const text = ScrollReveal({
+    origin: 'left',
+    distance:'100px',
+    duration:2000,
+    reset: true
+})
+text.reveal(`.about-img, .left, .bio-text, .form `,{
+    interval: 200
+})
